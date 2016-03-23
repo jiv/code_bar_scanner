da23309314552e6c12812e7f09db229d5050d8db
José Ignacio Varela <jivarela26@gmail.com>
2016-03-22 21:03:41 -0300

    Add Export dialog.

--- app/src/main/java/com/jivarela/codebarscanner/MainActivity.java
+++ app/src/main/java/com/jivarela/codebarscanner/MainActivity.java
@@ -1,15 +1,21 @@
 package com.jivarela.codebarscanner;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -64,7 +70,11 @@ public class MainActivity extends Activity {
 
         export_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                export(v);;
+                if (items_ok()) {
+                    export(v);
+                } else {
+                    showToast("There is an item without quantity.");
+                }
             }
         });
     }
@@ -95,8 +105,42 @@ public class MainActivity extends Activity {
     }
 
     public void export(View view){
+        if (items.isEmpty()){
+            showToast("There is no code to export.");
+        }else {
+            new AlertDialog.Builder(this)
+                    .setView(getLayoutInflater().inflate(R.layout.export_dialog, null))
+                    .setPositiveButton(R.string.export, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            Dialog d = (Dialog) dialog;
+                            EditText separator = (EditText) d.findViewById(R.id.separator);
+                            EditText company = (EditText) d.findViewById(R.id.company);
+                            if (!company.getText().toString().isEmpty()) {
+                                create_file(separator.getText().toString(), company.getText().toString());
+                            } else {
+                                showToast("Company can't be blank.");
+                            }
+                        }
+                    })
+                    .setNegativeButton(R.string.cancel, null)
+                    .show();
+        }
+    }
+
+    private boolean items_ok(){
+        boolean ok = true;
+        int i = 0;
+        while (ok && i < items.size()){
+            Product p = items.get(i);
+            ok = (!p.getCode().isEmpty() && !p.getQuantity().equals(0));
+            i++;
+        }
+        return ok;
+    }
+
+    private void create_file(String separator, String company){
         String root = Environment.getExternalStorageDirectory().toString();
-        File new_file = new File(root, "codigos_" + current_date() + ".txt");
+        File new_file = new File(root, company + "_" + current_date() + ".txt");
 
         try {
             FileOutputStream f = new FileOutputStream(new_file);
@@ -104,7 +148,7 @@ public class MainActivity extends Activity {
 
             for (int i = 0; i < items.size(); i++) {
                 Product p = items.get(i);
-                pw.println(p.getCode() + ',' + p.getQuantity());
+                pw.println(p.getCode() + separator + p.getQuantity());
             }
 
             pw.flush();
@@ -116,5 +160,9 @@ public class MainActivity extends Activity {
 
         Toast toast = Toast.makeText(this, "File exported to " + new_file.getAbsolutePath(), Toast.LENGTH_LONG);
         toast.show();
+    }
+
+    public void showToast(String msj){
+        Toast.makeText(this, msj, Toast.LENGTH_SHORT).show();
     }
 }
