98dbb73722e8e18cd75a93e8eda4cb9277b7342c
José Ignacio Varela <jivarela26@gmail.com>
2016-04-06 22:06:08 -0300

    Buttons on bottom.

--- app/src/main/java/com/jivarela/codebarscanner/MainActivity.java
+++ app/src/main/java/com/jivarela/codebarscanner/MainActivity.java
@@ -1,6 +1,7 @@
 package com.jivarela.codebarscanner;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -43,12 +45,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        for (int i =0; i<=15; i++){
-            Product p = new Product(String.valueOf(i));
-            p.setQuantity(i);
-            items.add(p);
-        }
-
         ButterKnife.bind(this);
 
         mRecyclerView.setHasFixedSize(true);
@@ -58,6 +54,16 @@ public class MainActivity extends AppCompatActivity {
 
         mAdapter = new ItemAdapter(this, items);
         mRecyclerView.setAdapter(mAdapter);
+
+//        mRecyclerView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+//            @Override
+//            public void onFocusChange(View v, boolean hasFocus) {
+//                if (hasFocus){
+//                    InputMethodManager imm = (InputMethodManager) (getSystemService(Context.INPUT_METHOD_SERVICE));
+//                    imm.hideSoftInputFromWindow(v.getApplicationWindowToken(), 0);
+//                }
+//            }
+//        });
 
         scan_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
